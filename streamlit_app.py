e22e9635da12dc316d0207fbd49860bb1c306af3
vayilanithinkumar <161498528+vayilanithinkumar@users.noreply.github.com>
2024-05-05 23:48:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,5 @@
 import numpy as np
 import pickle
-
-
-loaded_model= pickle.load(open('C:/Intel/Deploying ml model/machine learning/diabetes_model.sav','rb'))
-
-input_data = [10,168,74,0,0,38,0.537,34]
-input_data_as_numpy_array = np.asarray(input_data)
-
-input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-
-prediction = loaded_model.predict(input_data_reshaped)
-print(prediction)
-
-if (prediction[0]==0):
-    print('person is non diabetic')
-else:
-    print ('person is diabetic')
-
-
-
-import numpy as np
-import pickle
 import streamlit as st
 
 loaded_model= pickle.load(open('C:/Intel/Deploying ml model/machine learning/diabetes_model.sav','rb'))
